941a7f19c01f5e8cc9039ab68f85e798330ba911
wutiarn <wutiarn@wutiarn-nb.lan>
2016-02-28 12:26:36 +0300

    QR scanning

--- app/src/main/kotlin/ru/wutiarn/edustor/android/MainActivity.kt
+++ app/src/main/kotlin/ru/wutiarn/edustor/android/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.wutiarn.edustor.android
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
@@ -7,8 +8,14 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
+import com.google.zxing.integration.android.IntentIntegrator
 
 class MainActivity : AppCompatActivity() {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        IntentIntegrator.parseActivityResult(requestCode, resultCode, data)?.let {
+            Snackbar.make(findViewById(R.id.container), "Found ${it.contents}", Snackbar.LENGTH_LONG).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +24,10 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() }
+        //        fab.setOnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() }
+        fab.setOnClickListener {
+            IntentIntegrator(this).initiateScan(IntentIntegrator.QR_CODE_TYPES)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
