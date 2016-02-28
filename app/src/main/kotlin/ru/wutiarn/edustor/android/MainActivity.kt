f2e724e40a9b94a6bc73ff56a211314378b37cd9
wutiarn <wutiarn@wutiarn-nb.lan>
2016-02-28 14:59:23 +0300

    Flipboard bottomsheet

--- app/src/main/kotlin/ru/wutiarn/edustor/android/MainActivity.kt
+++ app/src/main/kotlin/ru/wutiarn/edustor/android/MainActivity.kt
@@ -6,8 +6,10 @@ import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
+import com.flipboard.bottomsheet.BottomSheetLayout
 import com.google.zxing.integration.android.IntentIntegrator
 
 class MainActivity : AppCompatActivity() {
@@ -28,6 +30,10 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener {
             IntentIntegrator(this).initiateScan(IntentIntegrator.QR_CODE_TYPES)
         }
+
+        val bottomSheetLayout = findViewById(R.id.bottomsheet) as BottomSheetLayout
+        val view = LayoutInflater.from(applicationContext).inflate(R.layout.sheet_document, bottomSheetLayout, false)
+        bottomSheetLayout.showWithSheetView(view)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
