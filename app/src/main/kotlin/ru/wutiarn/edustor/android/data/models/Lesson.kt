173237c0e2291ab84875d6b9c989907c6eb937ff
Dmitry Romanov <me@wutiarn.ru>
2016-07-24 21:48:44 +0300

    lesson date saving

--- app/src/main/kotlin/ru/wutiarn/edustor/android/data/models/Lesson.kt
+++ app/src/main/kotlin/ru/wutiarn/edustor/android/data/models/Lesson.kt
@@ -1,5 +1,6 @@
 package ru.wutiarn.edustor.android.data.models
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import io.realm.RealmList
 import io.realm.RealmObject
 import io.realm.annotations.Ignore
@@ -9,10 +10,18 @@ import org.bson.types.ObjectId
 import org.threeten.bp.LocalDate
 
 @RealmClass
-open class Lesson(): RealmObject()  {
+open class Lesson() : RealmObject() {
     open lateinit var subject: Subject
-    @Ignore open lateinit var date: LocalDate
+    @Ignore open var date: LocalDate = LocalDate.ofEpochDay(0)
+    get() = LocalDate.ofEpochDay(realmDate)
+    set(value) {
+        field = value
+        realmDate = value.toEpochDay()
+    }
     open var topic: String? = null
     open var documents: RealmList<Document> = RealmList()
     @PrimaryKey open var id: String = ObjectId.get().toString()
+
+    @JsonIgnore
+    private var realmDate: Long = 0
 }
