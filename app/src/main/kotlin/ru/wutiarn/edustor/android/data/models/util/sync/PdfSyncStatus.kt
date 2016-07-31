dacfb1f9860f93ce78eb93261f9ebf20bca574e6
Dmitry Romanov <me@wutiarn.ru>
2016-07-31 16:34:45 +0300

    #2 Make PdfSyncStatus.lesson nullable

--- app/src/main/kotlin/ru/wutiarn/edustor/android/data/models/util/sync/PdfSyncStatus.kt
+++ app/src/main/kotlin/ru/wutiarn/edustor/android/data/models/util/sync/PdfSyncStatus.kt
@@ -6,7 +6,7 @@ import ru.wutiarn.edustor.android.data.models.Lesson
 
 @RealmClass
 open class PdfSyncStatus() : RealmObject() {
-    lateinit open var lesson: Lesson
+    open var lesson: Lesson? = null
     open var markedForSync = false  // By user on LessonDetails
     open var synced = false
     open var fileLocation: String? = null
