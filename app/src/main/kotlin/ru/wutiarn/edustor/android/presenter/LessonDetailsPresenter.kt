4a574ecc72fe3353a6c72f5dfdff7fe180ff33d1
Dmitry Romanov <me@wutiarn.ru>
2016-08-08 13:22:55 +0300

    #15 Update realmValidUntil even if document is already synced

--- app/src/main/kotlin/ru/wutiarn/edustor/android/presenter/LessonDetailsPresenter.kt
+++ app/src/main/kotlin/ru/wutiarn/edustor/android/presenter/LessonDetailsPresenter.kt
@@ -84,15 +84,15 @@ class LessonDetailsPresenter(val appComponent: AppComponent, val context: Contex
     }
 
     fun onGetPdfClicked() {
+        Realm.getDefaultInstance().use {
+            it.executeTransaction {
+                lesson?.syncStatus!!.setShouldBeSynced(true)  // Update realmValidUntil even if document is already synced
+            }
+        }
         if (lesson!!.syncStatus!!.getStatus(lesson!!, appComponent.context) == PdfSyncStatus.SyncStatus.SYNCED) {
             openSyncedPdf()
         } else {
             openPdfAfterSyncFinished = true
-            Realm.getDefaultInstance().use {
-                it.executeTransaction {
-                    lesson?.syncStatus!!.setShouldBeSynced(true)
-                }
-            }
             appComponent.pdfSyncManager.requestSync(true)
             appComponent.eventBus.makeSnack("Pdf sync requested")
         }
