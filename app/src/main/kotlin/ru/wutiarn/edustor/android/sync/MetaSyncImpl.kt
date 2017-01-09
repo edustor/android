f82420bc31f42bcd11841257847b42ba1d2447d4
Dmitry Romanov <me@wutiarn.ru>
2017-01-09 20:12:47 +0300

    Move MetaSync impl to separate class

---
+++ app/src/main/kotlin/ru/wutiarn/edustor/android/sync/MetaSyncImpl.kt
@@ -0,0 +1,67 @@
+package ru.wutiarn.edustor.android.sync
+
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Handler
+import android.support.v4.app.NotificationCompat
+import android.util.Log
+import ru.wutiarn.edustor.android.R
+import ru.wutiarn.edustor.android.dagger.component.AppComponent
+import ru.wutiarn.edustor.android.events.EdustorMetaSyncFinished
+import ru.wutiarn.edustor.android.util.extension.fullSyncNow
+
+class MetaSyncImpl(val context: Context,
+                   val appComponent: AppComponent,
+                   val notificationService: NotificationManager,
+                   val notificationId: Int) {
+    val TAG = "SyncAdapter.Meta"
+    val handler = Handler(context.mainLooper)
+
+    val notificationBuilder: NotificationCompat.Builder
+        get() = NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.ic_cached_black_24dp)
+                .setContentTitle("Edustor Sync")
+                .setProgress(0, 0, true)
+
+    fun syncMeta(uploadOnly: Boolean) {
+        val tasks = appComponent.syncManager.popAllTasks()
+        val tasksCount = tasks.count()
+        Log.i(TAG, "Sync $tasksCount changes. Full: ${!uploadOnly}")
+
+        notificationService.notify(notificationId,
+                notificationBuilder
+                        .setContentText("Meta: Pushing $tasksCount changes")
+                        .build()
+        )
+
+        appComponent.api.sync.push(tasks)
+                .toBlocking()
+                .subscribe({ pushResult ->
+                    val tasksSucceeded = pushResult.count { it.success }
+                    val msgStr = "Task push succeeded: $tasksSucceeded/${tasks.size}"
+                    Log.i(TAG, msgStr)
+
+                    if (!uploadOnly) {
+
+                        notificationService.notify(notificationId,
+                                notificationBuilder
+                                        .setContentText("Meta: Fetching backend data")
+                                        .build()
+                        )
+
+                        appComponent.api.sync.fullSyncNow()
+                                .toBlocking()
+                                .subscribe({
+                                    handler.post {
+                                        appComponent.eventBus.post(EdustorMetaSyncFinished(true))
+                                    }
+                                }, {
+                                    throw SyncException("Fetch failed: $it")
+                                })
+                    }
+                    notificationService.cancel(notificationId)
+                }, {
+                    throw SyncException("Push failed: $it")
+                })
+    }
+}
--- app/src/main/kotlin/ru/wutiarn/edustor/android/sync/SyncAdapter.kt
+++ app/src/main/kotlin/ru/wutiarn/edustor/android/sync/SyncAdapter.kt
@@ -10,7 +10,6 @@ import android.util.Log
 import retrofit2.adapter.rxjava.HttpException
 import ru.wutiarn.edustor.android.R
 import ru.wutiarn.edustor.android.events.EdustorMetaSyncFinished
-import ru.wutiarn.edustor.android.util.extension.fullSyncNow
 import ru.wutiarn.edustor.android.util.extension.initializeNewAppComponent
 import java.io.IOException
 
@@ -22,64 +21,21 @@ class SyncAdapter(context: Context, autoInitialize: Boolean) : AbstractThreadedS
     val TAG = "SyncAdapter"
     val NOTIFICATION_ID = 0
 
-    val notificationBuilder: NotificationCompat.Builder
-        get() = NotificationCompat.Builder(context)
-                .setSmallIcon(R.drawable.ic_cached_black_24dp)
-                .setContentTitle("Edustor Sync")
-                .setContentText("Preparing sync...")
-                .setProgress(0, 0, true)
-
     override fun onPerformSync(account: Account?, extras: Bundle, authority: String?, provider: ContentProviderClient?, syncResult: SyncResult) {
 
         val uploadOnly = extras.getBoolean(ContentResolver.SYNC_EXTRAS_UPLOAD)
         val isManual = extras.getBoolean(ContentResolver.SYNC_EXTRAS_MANUAL)
 
+        val metaSyncImpl = MetaSyncImpl(context, appComponent, notificationService, NOTIFICATION_ID)
+
         try {
-            syncMeta(uploadOnly)
+            metaSyncImpl.syncMeta(uploadOnly)
         } catch (e: SyncException) {
             handleSyncException(e, syncResult)
             handler.post {
                 appComponent.eventBus.post(EdustorMetaSyncFinished(false, e))
             }
         }
-    }
-
-    fun syncMeta(uploadOnly: Boolean) {
-
-        val tasks = appComponent.syncManager.popAllTasks()
-
-        val tasksCount = tasks.count()
-        Log.i(TAG, "Sync $tasksCount changes. Full: ${!uploadOnly}")
-
-        notificationService.notify(NOTIFICATION_ID,
-                notificationBuilder
-                        .setContentText("Meta: Pushing $tasksCount changes")
-                        .build()
-        )
-
-        appComponent.api.sync.push(tasks)
-                .toBlocking()
-                .subscribe({ pushResult ->
-                    val tasksSucceeded = pushResult.count { it.success }
-                    val msgStr = "Task push succeeded: $tasksSucceeded/${tasks.size}"
-                    Log.i(TAG, msgStr)
-
-                    if (!uploadOnly) {
-                        appComponent.api.sync.fullSyncNow()
-                                .toBlocking()
-                                .subscribe({
-                                    handler.post {
-                                        appComponent.eventBus.post(EdustorMetaSyncFinished(true))
-                                    }
-                                }, {
-                                    throw SyncException("Fetch failed: $it")
-                                })
-                    }
-
-                    notificationService.cancel(NOTIFICATION_ID)
-                }, {
-                    throw SyncException("Push failed: $it")
-                })
     }
 
     private fun handleSyncException(ex: SyncException, syncResult: SyncResult) {
