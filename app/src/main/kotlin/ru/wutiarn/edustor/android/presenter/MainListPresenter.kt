3b7fd2ca797dcd33594f61de7a80a6acd867513a
Dmitry Romanov <me@wutiarn.ru>
2017-03-13 17:40:08 +0300

    WIP: Remove rx.Observable from repositories
    Fix MainListPresenter

--- app/src/main/kotlin/ru/wutiarn/edustor/android/presenter/MainListPresenter.kt
+++ app/src/main/kotlin/ru/wutiarn/edustor/android/presenter/MainListPresenter.kt
@@ -1,7 +1,6 @@
 package ru.wutiarn.edustor.android.presenter
 
 import android.app.DatePickerDialog
-import android.util.Log
 import android.widget.DatePicker
 import com.hannesdorfmann.mosby.mvp.MvpPresenter
 import io.realm.Realm
@@ -10,18 +9,11 @@ import ru.wutiarn.edustor.android.dagger.component.AppComponent
 import ru.wutiarn.edustor.android.data.models.Lesson
 import ru.wutiarn.edustor.android.data.models.MainListEntity
 import ru.wutiarn.edustor.android.data.models.Tag
-import ru.wutiarn.edustor.android.events.RequestSnackbarEvent
-import ru.wutiarn.edustor.android.util.extension.linkToLCEView
 import ru.wutiarn.edustor.android.view.MainListView
-import rx.Observable
-import rx.subscriptions.CompositeSubscription
 
 class MainListPresenter(val appComponent: AppComponent, val parentTagId: String?) : MvpPresenter<MainListView>,
         DatePickerDialog.OnDateSetListener {
     var view: MainListView? = null
-
-    var activeSubscription: CompositeSubscription? = null
-    private val entityCache: MutableMap<EntityType, List<MainListEntity>> = mutableMapOf()
 
     private enum class EntityType {
         TAGS,
@@ -31,7 +23,6 @@ class MainListPresenter(val appComponent: AppComponent, val parentTagId: String?
     override fun detachView(p0: Boolean) {
         view = null
         appComponent.eventBus.unregister(this)
-        activeSubscription?.unsubscribe()
     }
 
     override fun attachView(p0: MainListView?) {
@@ -40,39 +31,30 @@ class MainListPresenter(val appComponent: AppComponent, val parentTagId: String?
     }
 
     fun loadData() {
-        activeSubscription?.unsubscribe()
-        activeSubscription = CompositeSubscription()
-        entityCache.clear()
+        parentTagId?.let {
+            val tag = appComponent.repo.tag.byId(parentTagId)
+            view?.setTitle(tag.name)
+        }
+
+
+        val entities = mutableListOf<MainListEntity>()
 
         EntityType.values().forEach { entityType ->
-            parentTagId?.let {
-                val tag = appComponent.repo.tag.byId(parentTagId)
-                view?.setTitle(tag.name)
-            }
-
-            @Suppress("UNCHECKED_CAST")
-            val observable: Observable<List<MainListEntity>> = when (entityType) {
-                EntityType.TAGS -> appComponent.repo.tag.byParentTagId(parentTagId).sortedBy(Tag::name) as Observable<List<MainListEntity>>
+            val typeEntities = when (entityType) {
+                EntityType.TAGS -> appComponent.repo.tag.byParentTagId(parentTagId).sortedBy(Tag::name)
                 EntityType.LESSONS -> {
                     parentTagId ?: return@forEach
                     appComponent.repo.lessons.byTagId(parentTagId)
                             .filter { it.pages.size > 0 }
-                            .sortedByDescending(Lesson::date) as Observable<List<MainListEntity>>
+                            .sortedByDescending(Lesson::date)
                 }
-                else -> throw IllegalStateException("Unsupported entity type") // (Almost) impossible :)
             }
 
-            val subscription = observable
-                    .map {
-                        entityCache[entityType] = it
-
-                        EntityType.values()
-                                .filter { it in entityCache }
-                                .flatMap { entityCache[it] ?: emptyList() }
-                    }
-                    .linkToLCEView(view)
-            activeSubscription!!.add(subscription)
+            @Suppress("UNCHECKED_CAST")
+            entities.addAll(typeEntities as List<MainListEntity>)
         }
+
+        view?.setData(entities)
     }
 
 
